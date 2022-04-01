9a2e1887fe8f3d3b150cf19724d82c42324ebd71
Elizaveta Kabak <kabakelizaveta@gmail.com>
2022-04-01 14:58:28 +0300

    task(Lesson5_3) done in to steps

--- src/com/company/Lesson5_DoubleArrays/Lesson5_3.java
+++ src/com/company/Lesson5_DoubleArrays/Lesson5_3.java
@@ -2,25 +2,36 @@ package com.company.Lesson5_DoubleArrays;
 
 public class Lesson5_3 {
     public static void main(String[] args) {
-        int [][] arrayTwo = Lesson5_1.inputArrayWithRandom(9,9,0,10);
+        int n = 9;
+        int m = 9;
+        int [][] arrayTwo = Lesson5_1.inputArrayWithRandom(n,m,0,10);
         Lesson5_1.printArray(arrayTwo);
         int min = arrayTwo[0][0];
-        for (int i = 0; i < arrayTwo.length; i++) {
+        for (int[] ints : arrayTwo) {
             for (int j = 0; j < arrayTwo[0].length; j++) {
-                if (arrayTwo[i][j] < min) {
-                    min = arrayTwo[i][j];
+                if (ints[j] < min) {
+                    min = ints[j];
                 }
             }
         }
-        System.out.println();
-        System.out.println(min);
         for (int i = 0; i < arrayTwo.length ; i++) {
             for (int j = 0; j < arrayTwo[0].length; j++) {
                 if(arrayTwo[i][j] == min) {
-                    System.out.print(i + " ");
+                    System.out.print("Все индексы минимального элемента: " + i + " ");
                     System.out.println(j);           // все индексы
                 }
             }
         }
+        int minIndexN = 0;
+        int minIndexM = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arrayTwo[i][j] < arrayTwo[minIndexN][minIndexM]) {
+                    minIndexN = i;
+                    minIndexM = j;
+                }
+            }
+        }
+        System.out.println("Индекс первого найденного минимального: " + minIndexN + " " + minIndexM);
     }
 }
--- src/com/company/Lesson5_DoubleArrays/Lesson5_4.java
+++ src/com/company/Lesson5_DoubleArrays/Lesson5_4.java
@@ -2,13 +2,15 @@ package com.company.Lesson5_DoubleArrays;
 
 public class Lesson5_4 {
     public static void main(String[] args) {
-        int [][] arrayTwo = Lesson5_1.inputArrayWithRandom(9,9,0,10);
+        int n = 9;
+        int m = 9;
+        int [][] arrayTwo = Lesson5_1.inputArrayWithRandom(n,m,0,10);
         Lesson5_1.printArray(arrayTwo);
         int max = arrayTwo[0][0];
-        for (int i = 0; i < arrayTwo.length; i++) {
+        for (int[] ints : arrayTwo) {
             for (int j = 0; j < arrayTwo[0].length; j++) {
-                if (arrayTwo[i][j] > max) {
-                    max = arrayTwo[i][j];
+                if (ints[j] > max) {
+                    max = ints[j];
                 }
             }
         }
@@ -17,10 +19,21 @@ public class Lesson5_4 {
         for (int i = 0; i < arrayTwo.length ; i++) {
             for (int j = 0; j < arrayTwo[0].length; j++) {
                 if(arrayTwo[i][j] == max) {
-                    System.out.print(i + " ");
+                    System.out.print("Все индексы максимального элемента: " + i + " ");
                     System.out.println(j);           // все индексы
                 }
             }
         }
+        int maxIndexN = 0;
+        int maxIndexM = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arrayTwo[i][j] > arrayTwo[maxIndexN][maxIndexM]) {
+                    maxIndexN = i;
+                    maxIndexM = j;
+                }
+            }
+        }
+        System.out.println("Индекс первого найденного максимального: " + maxIndexN + " " + maxIndexM);
     }
 }

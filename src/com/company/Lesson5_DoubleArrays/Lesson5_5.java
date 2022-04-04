67a67851a556c7bfc2c2ede02a87bca7e7d3f5f9
Elizaveta Kabak <kabakelizaveta@gmail.com>
2022-04-04 10:24:21 +0300

    Lesson 5_5 is done

--- src/com/company/Lesson5_DoubleArrays/Lesson5_5.java
+++ src/com/company/Lesson5_DoubleArrays/Lesson5_5.java
@@ -2,57 +2,43 @@ package com.company.Lesson5_DoubleArrays;
 
 public class Lesson5_5 {
     public static void main(String[] args) {
+        int n = 9;
+        int m = 9;
+        int[][] arrayTwo = Lesson5_1.inputArrayWithRandom(n, m, 0, 10);
+        Lesson5_1.printArray(arrayTwo);
+        System.out.println();
+        Task(arrayTwo, n, m);
+    }
+    public static void Task(int[][] arr, int n, int m) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (isLocalMax(arr, n, m, i, j)) {
+                    count++;
+                   System.out.println(count + "число = " + arr[i][j]);
+                   System.out.println("Индекс: " + i + " " + j);
+                }
+            }
+        }
+        System.out.println(count);
+        if (count == 0) {
+            System.out.println("Чисел нет");
         }
     }
-//        int n = 9;
-//        int m = 9;
-//        int[][] arrayTwo = Lesson5_1.inputArrayWithRandom(n, m, 0, 10);
-//        Lesson5_1.printArray(arrayTwo);
-//        System.out.println();
-////        Task(arrayTwo, n, m);
-//        int count = 0;
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < m; j++) {
-//                if ( isLocalMax(arrayTwo,n,m,i,j)) {
-//                    count++;
-//                    System.out.println(count + "число = " + arrayTwo[i][j]);
-//                    System.out.println("Индекс: " + i + " " + j);
-//                }
-//            }
-//        }
-//        if (count == 0) {
-//            System.out.println("Чисел нет");
-//        }
-//    }
-//    public static boolean isLocalMax ( int [][] arr, int n, int m, int i, int m) {
-//        int [][] arr = new int [n][m];
-//
-//    }
-
-//    public static void Task (int [][] arr, int n, int m) {
-//        boolean chek;
-//        int count  = 0;
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < m; j++) {
-//                chek = true;
-//                for (int f = i-1; f <= i+1 ; f++) {
-//                    for (int k = j-1; k <= n ; k++) {
-//                        if (f >= 0 && k >= 0 && f<n && k <m && !(f==i && k == j)) {
-//                            if (arr[f][k] >= arr[i][j]) {
-//                                chek = false;
-//                                break;
-//                            }
-//                        }
-//                    }
-//                }
-//                if (chek) {
-//                    count++;
-//                    System.out.println(count + "число = " + arr[i][j]);
-//                    System.out.println("Индекс " + i + " " + j);
-//                }
-//            }
-//        }
-//        if(count == 0) {
-//            System.out.println("Чисел нет");
-//        }
-//    }
+    public static boolean isLocalMax(int[][] arr, int n, int m, int i, int j) {
+        int startI = i > 0 ? i - 1 : 0;
+        int endI = i < n - 1 ? i + 1 : n - 1;
+        int startJ = j > 0 ? j - 1 : 0;
+        int endJ = j < m - 1 ? j + 1 : m - 1;
+        for (int f = startI; f <=  endI; f++) {
+            for (int k = startJ; k <= endJ; k++) {
+                if (!(f == i && k == j)) {
+                    if(arr[f][k] >= arr[i][j]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}

52e5330fbaeebc45908cff8228a6f2a655f14e1a
Elizaveta Kabak <kabakelizaveta@gmail.com>
2022-04-04 15:43:25 +0300

    Codewars task 1 is done

--- src/com/company/codeWars/task1CodeWars.java
+++ src/com/company/codeWars/task1CodeWars.java
@@ -4,21 +4,38 @@ import java.util.Arrays;
 
 public class task1CodeWars {
     public static void main(String[] args) {
-        int arr[] = {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20};
+        int [] arr = {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20};
         //нужно получить : "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
-//        int count = 1;
-//        String lineNew = "";
-//        String tr = "-";
-//        for (int i = 0; i < arr.length; i++) {
-//            while (arr[i] + 1 == arr[i+1]) {
-//                count++;
-//            }   if (count < 3) {
-//                lineNew = lineNew + arr[i] + ", ";
-//            }
-//            if (count >= 3) {
-//                lineNew = lineNew + tr + arr[i + 1] + ", ";
-//            }
-//        }
-//        System.out.println(lineNew);
+        System.out.println(rangeExtraction(arr));
+    }
+    public static String rangeExtraction(int[] arr) {
+        boolean flag = false;
+        StringBuilder lineNew = new StringBuilder();
+        String tr = "-";
+        for (int i = 0; i < arr.length - 1;i ++) {
+            int count = 1;
+            lineNew.append(arr[i]);
+            while (arr[i] + 1 == arr[i + 1]) {
+                count ++;
+                i++;
+                if (i == arr.length - 1) {
+                    flag = true;
+                    break;
+                }
+            }
+                if (flag) {
+                    lineNew.append(tr).append(arr[i]);
+                    break;
+                }
+                if (count >= 3) {
+                    lineNew.append(tr).append(arr[i]).append(", ");
+                } else if (count == 2) {
+                    i--;
+                    lineNew.append(", ");
+                } else {
+                    lineNew.append(", ");
+                }
+            }
+        return lineNew.toString();
     }
 }

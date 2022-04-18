60b8c56ccb82ce0e313991d98ee247883697feec
Elizaveta Kabak <kabakelizaveta@gmail.com>
2022-04-18 16:46:24 +0300

    Home work WriteToFile

--- src/com/company/Figures/Point.java
+++ src/com/company/Figures/Point.java
@@ -1,8 +1,9 @@
 package com.company.Figures;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Point {
+public class Point implements Serializable {
     protected double x;
     protected double y;
 
--- src/com/company/Figures/Triangle.java
+++ src/com/company/Figures/Triangle.java
@@ -1,5 +1,6 @@
 package com.company.Figures;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class Triangle extends Figure {
